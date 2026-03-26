4e514559fa9bf724eedb6826bcc134c3de613a99
Jeevitha-98 <jeevithakarunanithi98@gmai.l.com>
2026-03-26 17:48:00 +0530

    ReactJS task4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-//import "./App.css";
+import "./App.css";
 import Parent from "./Parent";
 import Map from "./Component/Map"
+import Studentcard from "./Studentcard";
 
-
-function App() {
+export default function App() {
+  const students=[
+{id: "EHC001", name:"Jeevitha", Course:"Data Analytics", isActive:true},
+{id: "EHC002", name:"Gokul", Course:"Back-End Developer", isActive:true},
+{id: "EHC003", name:"Deepu", Course:"Front-End Developer", isActive:false},
+{id: "EHC004", name:"Akash", Course:"Software Tester", isActive:true},
+{id: "EHC005", name:"Kannan", Course:"Software Engineer", isActive:false},
+  ];
+  
   return (
-    <div>
-      <Map/>
+    <div className="container">
+      <h1>🎓 Student Dashboard</h1>
+      <p className="count">Total Students: {students.length}</p>
+
+      <div className="card-container">
+        {students.map((student) => (
+          <Studentcard key={student.id} student={student} />
+        ))}
+    
+    </div>
     </div>
   );
 }
 
-export default App;
+
 
    
