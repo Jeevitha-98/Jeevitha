e5c24c248b58791264dda989a3e60bcf158543d3
Jeevitha-98 <jeevithakarunanithi98@gmai.l.com>
2026-04-06 17:11:26 +0530

    ReactJS task 9

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@
 import RegistrationForm from "./Component/Registration form";
 import Stopwatch from "./Component/Stopwatch";
 import API from "./Component/API";
-import Minichatapp from "./Component/Minichatapp";
+//import Minichatapp from "./Component/Minichatapp";
+ import React, { useState } from "react";
+import { ThemeContext } from "./Component/ThemeContext";
+import Profile from "./Component/Profile";
+
 
 
 /*export default function App() {
@@ -33,11 +37,59 @@ import Minichatapp from "./Component/Minichatapp";
 }*/
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+const appStyle = {
+  textAlign: "center",
+  padding: "40px 20px",
+  minHeight: "100vh",
+  transition: "0.3s",
+  background:
+    theme === "light"
+      ? "linear-gradient(to right, #e3f2fd, #fce4ec)"
+      : "linear-gradient(to right, #1e1e2f, #121212)",
+  color: theme === "light" ? "#000" : "#faf5f5",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+const titleStyle = {
+  fontSize: "28px",
+  marginBottom: "20px",
+  color: theme === "light" ? "#d32f2f" : "#ff6b6b", 
+};
+
+const buttonStyle = {
+  padding: "10px 20px",
+  marginBottom: "50px",
+  border: "none",
+  background: "linear-gradient(45deg, #4caf50, #2e7d32)",
+  color: theme === "light" ? "#fff" : "#fff",
+  borderRadius: "25px",
+  cursor: "pointer",
+};
+
   return (
-    <div>
-      <Minichatapp/>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div style={appStyle}>
+        <h1 style={{ color: theme === "light" ? "red" : "#ff6b6b" }}>
+          Profile Upload & Theme Change</h1>
+
+        <button onClick={toggleTheme} style={buttonStyle}>
+          Toggle Theme
+        </button>
+
+        <Profile />
+      </div>
+    </ThemeContext.Provider>
   );
 }
+  
+
 
 
